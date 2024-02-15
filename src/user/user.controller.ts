ae8f92f12c24ecd89f3a5f83568e4424f712d65b
Mohd Afjal <mohd.afjal@primathon.in>
2024-02-15 19:08:23 +0530

    minor bug fix

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,7 +10,7 @@ import {
   Req,
   UseGuards,
 } from '@nestjs/common';
-import { UserDto } from './dto';
+import { StudentDto } from './dto';
 import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
 
@@ -22,17 +22,13 @@ export class UserController {
   @Get('info/:id')
   // @UseGuards(AuthGuard('jwt'))
   //   @UseGuards(JwtGuard)
-  getUserInfo(
-    @Body() dto: UserDto,
-    @Req() req: Request,
-    @Param('id') id: number,
-  ) {
-    console.log(req.user, typeof req.user);
+  getInfo(@Req() req: Request, @Param('id') id: number) {
+    // console.log("------------------------->",req.user," :::::::::: ",req.user["email"],":::::::::::::::");
     // const userInfo = req.user;
     //userInfo.email   === { sub: 6, email: 'efg@gmail.com', iat: 1707394707, exp: 1707395607 }
     // trying to access email from req.user ... Not accessible , since it's not a top-level key..
     // use .attributes. to fetch other keys information..
-
+    const userEmail = req.user['email'];
     // console.log(req.user.constructor, req.user.constructor.prototype);
     // let val = '';
     // for (val of Object.keys(req.user)) {
@@ -46,34 +42,48 @@ export class UserController {
     //   );
     // }
 
-    console.log('Inside getUserInfo !', dto.email, dto.roles);
-    const msg = this.userService.get(dto, id);
+    console.log('Inside getUserInfo !', userEmail, ' and id ', id);
+    const msg = this.userService.get(userEmail, id);
     return msg;
   }
 
   @Get('all')
-  getAll(@Body() dto: UserDto) {
-    return this.userService.getAll(dto);
+  getAll(@Req() req: Request) {
+    const userEmail = req.user['email'];
+    console.log('Inside getAll UserInfo !', userEmail);
+    return this.userService.getAll(userEmail);
   }
 
   @Post('create')
-  createUser(@Body() dto: UserDto) {
-    console.log('Inside post request createUser method ! ', dto);
-    return this.userService.create(dto);
+  create(@Req() req: Request, @Body() dto: StudentDto) {
+    const userEmail = req.user['email'];
+    console.log('create body', dto);
+    console.log('Inside create student info !', userEmail);
+    return this.userService.create(dto, userEmail);
   }
 
   @Put('update/:id')
-  updateUser(@Body() dto: UserDto, @Param('id') id: number) {
-    return this.userService.update(dto, id);
+  update(
+    @Body() dto: StudentDto,
+    @Param('id') id: number,
+    @Req() req: Request,
+  ) {
+    const userEmail = req.user['email'];
+    console.log('Inside update student Info !', userEmail, ' and id ', id);
+    return this.userService.update(dto, id, userEmail);
   }
 
   @Delete('delete/:id')
-  deleteUser(@Body() dto: UserDto, @Param('id') id: number) {
-    return this.userService.delete(dto, id);
+  delete(@Param('id') id: number, @Req() req: Request) {
+    const userEmail = req.user['email'];
+    console.log('Inside delete student info !', userEmail, ' and id ', id);
+    return this.userService.delete(userEmail, id);
   }
 
   @Delete('deleteAll')
-  deleteAll(@Body() dto: UserDto) {
-    return this.userService.deleteAll(dto);
+  deleteAll(@Req() req: Request) {
+    const userEmail = req.user['email'];
+    console.log('Inside deleteAll student Info !', userEmail);
+    return this.userService.deleteAll(userEmail);
   }
 }
